a4e529341aabbca3dcd90f478e34658a668631e5
Kenya SUZUKI <kenya.suzuki@outlook.com>
2016-12-05 00:18:05 +0900

    簡単なテストプログラムを作成

---
+++ TestCS1/CyclicAccess.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestCS1
+{
+    partial class CyclicAccess
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webView = new System.Windows.Forms.WebBrowser();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.checkAccess = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webView.Location = new System.Drawing.Point(0, 200);
+            this.webView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView.Name = "webView";
+            this.webView.ScriptErrorsSuppressed = true;
+            this.webView.Size = new System.Drawing.Size(284, 261);
+            this.webView.TabIndex = 0;
+            this.webView.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webView_DocumentCompleted);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.OnTick);
+            // 
+            // checkAccess
+            // 
+            this.checkAccess.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkAccess.AutoSize = true;
+            this.checkAccess.Location = new System.Drawing.Point(21, 29);
+            this.checkAccess.Name = "checkAccess";
+            this.checkAccess.Size = new System.Drawing.Size(51, 22);
+            this.checkAccess.TabIndex = 1;
+            this.checkAccess.Text = "アクセス";
+            this.checkAccess.UseVisualStyleBackColor = true;
+            // 
+            // CyclicAccess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 461);
+            this.Controls.Add(this.checkAccess);
+            this.Controls.Add(this.webView);
+            this.MaximizeBox = false;
+            this.Name = "CyclicAccess";
+            this.Text = "CyclicAccess";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webView;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox checkAccess;
+    }
+}
+
---
+++ TestCS1/CyclicAccess.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestCS1
+{
+    public partial class CyclicAccess : Form
+    {
+        private HtmlDocument doc= null;
+        private int n = 0;
+
+        public CyclicAccess()
+        {
+            InitializeComponent();
+            //webView.Navigate("http://www5.airnet.ne.jp/kenya/");
+            //webView.DocumentText = "<html><head><title>AA</title></head><body>ABC</body></html>";
+            Uri url = new Uri(Path.Combine(Environment.CurrentDirectory, @"HTMLPage1.html"));
+            webView.Navigate(url);
+            doc = webView.Document;
+
+            //String str = Properties.Settings.Default.Filename;
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            //aes.BlockSize = 128;
+            //aes.KeySize = 128;
+            var ivBlock = new byte[aes.BlockSize/8];
+            if (Properties.Settings.Default.IV.CompareTo("") == 0)
+            {
+                //var ivBlockRfc = new Rfc2898DeriveBytes("IVの初期値です", Encoding.UTF8.GetBytes("saltというキー"));
+                //ivBlock = ivBlockRfc.GetBytes(aes.BlockSize/8);
+                aes.GenerateIV();
+                ivBlock = aes.IV;
+                Properties.Settings.Default.IV = Convert.ToBase64String(ivBlock);
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                ivBlock = Convert.FromBase64String(Properties.Settings.Default.IV);
+            }
+            var keyRfc = new Rfc2898DeriveBytes("password", Encoding.UTF8.GetBytes("saltというキー"));
+            var key = keyRfc.GetBytes(aes.BlockSize/8);
+            var key_str = Convert.ToBase64String(key);
+            ICryptoTransform enc = aes.CreateEncryptor(key, ivBlock);
+            byte[] src = Encoding.UTF8.GetBytes("暗号化されるデータ");
+            var edata = enc.TransformFinalBlock(src, 0, src.Length);
+            var edata_b64 = Convert.ToBase64String(edata);
+            ICryptoTransform dec = aes.CreateDecryptor(key, ivBlock);
+            var data = dec.TransformFinalBlock(edata, 0, edata.Length);
+            var data_b64 = Convert.ToBase64String(data);
+            var data_str = Encoding.UTF8.GetString(data);
+
+            timer.Interval = 1000; //ms
+            timer.Start();
+        }
+
+        private void webView_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webView.DocumentText = "BBB";
+            //doc.BackColor = Color.FromArgb(0,255,255);
+            doc.GetElementById("UserID").InnerText = "user@id";
+            //doc.GetElementsByTagName("form")[0].AttachEventHandler("onsubmit", form_Submit);
+            //doc.GetElementById("Submit1").InvokeMember("click");
+            //doc.GetElementsByTagName("form")[0].InvokeMember("submit");
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            n++;
+            if (checkAccess.Checked) return;
+
+            HtmlElement elem = doc.CreateElement("p");
+            elem.InnerText = n.ToString();
+            doc.Body.AppendChild(elem);
+
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(new Point(0, 0), new Point(0, 0), bmp.Size);
+            g.Dispose();
+            //bmp.Save(Properties.Settings.Default.Filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+            bmp.Save(Properties.Settings.Default.Filename, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void form_Submit(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
